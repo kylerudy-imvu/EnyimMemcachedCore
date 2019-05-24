330348bd4a628508ea762a90e1d3bc1d402639b2
dudu <dudu@cnblogs.com>
2019-05-24 17:59:58 +0800

    Change file encoding

--- Enyim.Caching/Memcached/StoreMode.cs
+++ Enyim.Caching/Memcached/StoreMode.cs
@@ -1,58 +1,58 @@
 
 namespace Enyim.Caching.Memcached
 {
-	/// <summary>
-	/// Inidicates the mode how the items are stored in Memcached.
-	/// </summary>
-	public enum StoreMode
-	{
-		/// <summary>
-		/// Store the data, but only if the server does not already hold data for a given key
-		/// </summary>
-		Add = 1,
-		/// <summary>
-		/// Store the data, but only if the server does already hold data for a given key
-		/// </summary>
-		Replace,
-		/// <summary>
-		/// Store the data, overwrite if already exist
-		/// </summary>
-		Set
-	};
+    /// <summary>
+    /// Inidicates the mode how the items are stored in Memcached.
+    /// </summary>
+    public enum StoreMode
+    {
+        /// <summary>
+        /// Store the data, but only if the server does not already hold data for a given key
+        /// </summary>
+        Add = 1,
+        /// <summary>
+        /// Store the data, but only if the server does already hold data for a given key
+        /// </summary>
+        Replace,
+        /// <summary>
+        /// Store the data, overwrite if already exist
+        /// </summary>
+        Set
+    };
 
-	internal enum StoreCommand
-	{
-		/// <summary>
-		/// Store the data, but only if the server does not already hold data for a given key
-		/// </summary>
-		Add = 1,
-		/// <summary>
-		/// Store the data, but only if the server does already hold data for a given key
-		/// </summary>
-		Replace,
-		/// <summary>
-		/// Store the data, overwrite if already exist
-		/// </summary>
-		Set,
-		/// <summary>
-		/// Appends the data to an existing key's data
-		/// </summary>
-		Append,
-		/// <summary>
-		/// Inserts the data before an existing key's data
-		/// </summary>
-		Prepend,
-		/// <summary>
-		/// Stores the data only if it has not been updated by someone else. Uses a "transaction id" to check for modification.
-		/// </summary>
-		CheckAndSet
-	};
+    internal enum StoreCommand
+    {
+        /// <summary>
+        /// Store the data, but only if the server does not already hold data for a given key
+        /// </summary>
+        Add = 1,
+        /// <summary>
+        /// Store the data, but only if the server does already hold data for a given key
+        /// </summary>
+        Replace,
+        /// <summary>
+        /// Store the data, overwrite if already exist
+        /// </summary>
+        Set,
+        /// <summary>
+        /// Appends the data to an existing key's data
+        /// </summary>
+        Append,
+        /// <summary>
+        /// Inserts the data before an existing key's data
+        /// </summary>
+        Prepend,
+        /// <summary>
+        /// Stores the data only if it has not been updated by someone else. Uses a "transaction id" to check for modification.
+        /// </summary>
+        CheckAndSet
+    };
 }
 
 #region [ License information          ]
 /* ************************************************************
  * 
- *    Copyright (c) 2010 Attila Kiskó, enyim.com
+ *    Copyright (c) 2010 Attila Kisk? enyim.com
  *    
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
